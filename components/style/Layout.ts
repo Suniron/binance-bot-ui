6c96384cd3df6e3d1d32ffdd32c5132108306194
Etienne BLANC <etienne.blanc94@gmail.com>
2021-08-08 01:26:43 +0200

    fix padding

--- components/style/Layout.ts
+++ components/style/Layout.ts
@@ -4,8 +4,7 @@ const MainLayout = styled("div", {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  paddingLeft: "8px",
-  paddingRight: "8px",
+  padding: "8px",
 });
 
 export default MainLayout;
