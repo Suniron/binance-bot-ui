70285b686a1965f2568896012ac6a338b48ebe63
Etienne BLANC <etienne.blanc94@gmail.com>
2021-08-07 23:38:24 +0200

    📝 Add Github source link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,12 @@ import { styled } from "../stitches.config";
 const FooterDiv = styled("div", {});
 
 const Footer = () => {
-  return <FooterDiv>Interface developed by Etienne BLANC</FooterDiv>;
+  return (
+    <FooterDiv>
+      Interface developed by Etienne BLANC (
+      <a href="https://github.com/Suniron/binance-bot-ui">sources</a>)
+    </FooterDiv>
+  );
 };
 
 export default Footer;
