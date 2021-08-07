b25106a2bc0026079e76e4c450f8770fa9992d29
Etienne BLANC <etienne.blanc94@gmail.com>
2021-08-07 11:38:37 +0200

    🐛 Ignore same address in cache

--- utils/cache.utils.ts
+++ utils/cache.utils.ts
@@ -14,13 +14,18 @@ export const setCachedAddresses = (addresses: string[]) => {
 
 export const addCachedAddress = (address: string) => {
   const cachedAddresses = getCachedAddresses();
+
+  // If already in cache, ignore it:
+  if (cachedAddresses.find((a) => a === address)) {
+    return;
+  }
   cachedAddresses.push(address);
   setCachedAddresses(cachedAddresses);
 };
 
 export const removeCachedAddress = (address: string) => {
   const cachedAddresses = getCachedAddresses();
-  const indexToRemove = cachedAddresses.findIndex((c) => c === address);
+  const indexToRemove = cachedAddresses.findIndex((a) => a === address);
 
   if (!indexToRemove && indexToRemove !== 0) {
     return;
